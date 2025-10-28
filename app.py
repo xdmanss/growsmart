3d0c20ef41994fd848e1817a3a1127f58916682c
xdmanss <yousefhd334@gmail.com>
2025-10-28 17:35:09 +0400

    Fix Render dynamic port

--- app.py
+++ app.py
@@ -757,6 +757,6 @@ def server_error(e):
 ########################################
 
 if __name__ == "__main__":
-    # run dev server
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
